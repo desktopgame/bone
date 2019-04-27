71a66bad36dc5be54f2ef7a0cc7478d90fe89e70
Koya <koya0306@outlook.jp>
2019-04-27 22:48:36 +0900

    configure project

--- src/bone/main.c
+++ src/bone/main.c
@@ -4,21 +4,19 @@
 #include <glib.h>
 #include <stdio.h>
 
-static int add(int a, int b) { return a; }
-
-static void addTest() {
-        printf("test\n");
-        CU_ASSERT(add(0, 0) == 0);
-        CU_ASSERT(add(1, 0) == 1);
-        CU_ASSERT(add(1, 1) == 2);
+static int helloBone() {
+        printf("hello, bone\n");
+        return 0;
 }
+
+static void helloBoneTest() { CU_ASSERT(helloBone() == 0); }
 
 static void runTest() {
         CU_pSuite testSuite;
 
         CU_initialize_registry();
-        testSuite = CU_add_suite("addTestSuite", NULL, NULL);
-        CU_add_test(testSuite, "addTest", addTest);
+        testSuite = CU_add_suite("boneTestSuite", NULL, NULL);
+        CU_add_test(testSuite, "helloBoneTest", helloBoneTest);
         CU_basic_set_mode(CU_BRM_VERBOSE);
         CU_basic_run_tests();
         CU_cleanup_registry();
