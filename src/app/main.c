e0b75e9fdc4dfdf824dc14155947a49a0233b81b
Koya <koya0306@outlook.jp>
2019-04-27 19:44:21 +0900

    support CUnit

--- src/app/main.c
+++ src/app/main.c
@@ -1,10 +1,30 @@
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
+#include <CUnit/Console.h>
 #include <glib.h>
 #include <stdio.h>
 
+static int add(int a, int b) { return a; }
+
+static void addTest() {
+        printf("test\n");
+        CU_ASSERT(add(0, 0) == 0);
+        CU_ASSERT(add(1, 0) == 1);
+        CU_ASSERT(add(1, 1) == 2);
+}
+
+static void runTest() {
+        CU_pSuite testSuite;
+
+        CU_initialize_registry();
+        testSuite = CU_add_suite("addTestSuite", NULL, NULL);
+        CU_add_test(testSuite, "addTest", addTest);
+        CU_basic_set_mode(CU_BRM_VERBOSE);
+        CU_basic_run_tests();
+        CU_cleanup_registry();
+}
+
 int main(int argc, char *argv[]) {
-        GString *my_string =
-            g_string_new("This Hello world is %d chars long\n");
-        g_print(my_string->str, my_string->len);
-        g_string_free(my_string, TRUE);
+        runTest();
         return 0;
 }
