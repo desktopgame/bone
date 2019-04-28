4943635f11036de56453eb0393bb643013bfb90d
Koya <koya0306@outlook.jp>
2019-04-29 00:07:34 +0900

    glib test

--- src/bone/test.c
+++ src/bone/test.c
@@ -3,7 +3,17 @@
 #include <CUnit/CUnit.h>
 #include <CUnit/Console.h>
 #include "bone.h"
+#include "parse/parser.h"
 
-static int bnParse() { return 0; }
+static int bnParse(const char* dir) {
+        GError* err = NULL;
+        GDir* dirp = g_dir_open(dir, 0, &err);
+        gchar* file = ".";
+        while ((file = g_dir_read_name(dirp)) != NULL) {
+                g_printf("%s: f\n", file);
+        }
+        g_dir_close(dirp);
+        return 0;
+}
 
-void bnParseTest() { CU_ASSERT(bnParse() == 0); }
+void bnParseTest() { CU_ASSERT(bnParse("./testdata/parse/fail") == 0); }
