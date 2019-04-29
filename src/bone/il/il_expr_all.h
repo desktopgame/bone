ae93d2ff4da515f7ec0726bed1520837a8b989dd
Koya <koya0306@outlook.jp>
2019-04-29 23:16:57 +0900

    add header

---
+++ src/bone/il/il_expr_all.h
@@ -0,0 +1,9 @@
+#ifndef BONE_IL_ILEXPRALL_H
+#define BONE_IL_ILEXPRALL_H
+
+#include "il_expr_char.h"
+#include "il_expr_double.h"
+#include "il_expr_int.h"
+#include "il_expr_string.h"
+#include "il_expression.h"
+#endif
---
+++ src/bone/il/il_stmt_all.h
@@ -0,0 +1,8 @@
+#ifndef BONE_IL_ILSTMTALL_H
+#define BONE_IL_ILSTMTALL_H
+
+#include "il_statement.h"
+#include "il_stmt_expr_stmt.h"
+#include "il_stmt_if.h"
+#include "il_stmt_while.h"
+#endif
