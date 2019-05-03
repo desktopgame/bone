c939d3df4d2a96cec832d0a23248fb2843d5ea04
Koya <koya0306@outlook.jp>
2019-05-03 14:26:33 +0900

    add comment

--- src/bone/util/stack.h
+++ src/bone/util/stack.h
@@ -1,24 +1,58 @@
 #ifndef BONE_UTIL_STACK_H
 #define BONE_UTIL_STACK_H
 
+/**
+ * delete function for element in stack.
+ */
 typedef void (*bnStackElementDeleter)(void*);
 
+/**
+ * single link list.
+ */
 typedef struct bnStackElement {
         void* value;
         struct bnStack* next;
 } bnStackElement;
 
+/**
+ * stack.
+ */
 typedef struct bnStack {
         bnStackElement* head;
 } bnStack;
 
+/**
+ * return new instance of bnStack.
+ * @param type
+ * @return
+ */
 bnStack* bnNewStack();
 
+/**
+ * push to stack.
+ * @param self
+ * @param data
+ */
 void bnPushStack(bnStack* self, void* data);
 
+/**
+ * pop from stack.
+ * @param self
+ * @return
+ */
 void* bnPopStack(bnStack* self);
 
+/**
+ * return size of stack.
+ * @param self
+ * @return
+ */
 int bnGetStackSize(bnStack* self);
 
+/**
+ * free a bnStack.
+ * @param self
+ * @param deleter
+ */
 void bnDeleteStack(bnStack* self, bnStackElementDeleter deleter);
 #endif
