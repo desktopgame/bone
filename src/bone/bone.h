428e4f581be97382d86f7faa88e021078a291cab
Koya <koya0306@outlook.jp>
2019-05-03 12:08:20 +0900

    update header

--- src/bone/bone.h
+++ src/bone/bone.h
@@ -2,9 +2,9 @@
 #define BONE_BONE_H
 
 #include <assert.h>
-#include <glib-2.0/glib.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include "glib.h"
 #include "util/fmt.h"
 #include "util/memory.h"
 #include "util/string_pool.h"
---
+++ src/bone/glib.h
@@ -0,0 +1,4 @@
+#ifndef BONE_GLIB_H
+#define BONE_GLIB_H
+#include <glib-2.0/glib.h>
+#endif
