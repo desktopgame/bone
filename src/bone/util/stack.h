0cd67aafb7ac4177ad58fcbdb9cc3f6709ffbfb7
Koya <koya0306@outlook.jp>
2019-05-05 00:33:21 +0900

    fix member

--- src/bone/util/stack.h
+++ src/bone/util/stack.h
@@ -11,7 +11,7 @@ typedef void (*bnStackElementDeleter)(void*);
  */
 typedef struct bnStackElement {
         void* value;
-        struct bnStack* next;
+        struct bnStackElement* next;
 } bnStackElement;
 
 /**
