8d256b37ce842367add08132876c6e96c23539e0
Koya <koya0306@outlook.jp>
2019-05-10 14:18:16 +0900

    add comment

--- src/bone/util/fmt.h
+++ src/bone/util/fmt.h
@@ -2,7 +2,16 @@
 #define BONE_UTIL_FMT_H
 #include <stdio.h>
 
+/**
+ * call bnFindent with  stdout.
+ * @param depth
+ */
 void bnIndent(int depth);
 
+/**
+ * output spaces to file stream.
+ * @param fp
+ * @param depth
+ */
 void bnFindent(FILE* fp, int depth);
 #endif
