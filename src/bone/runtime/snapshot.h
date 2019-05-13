214fb7ee999164411b55f139eaae6ffebf1ca394
Koya <koya0306@outlook.jp>
2019-05-13 11:11:09 +0900

    add comment

--- src/bone/runtime/snapshot.h
+++ src/bone/runtime/snapshot.h
@@ -2,12 +2,24 @@
 #define BONE_RUNTIME_SNAPSHOT_H
 #include "../glib.h"
 
+/**
+ * bnSnapShot is save a state of frame at execution.
+ */
 typedef struct bnSnapShot {
         int pc;
         GHashTable* table;
 } bnSnapShot;
 
+/**
+ * return new instance of bnString.
+ * @param pc
+ * @return
+ */
 bnSnapShot* bnNewSnapShot(int pc);
 
+/**
+ * free a bnString.
+ * @param self
+ */
 void bnDeleteSnapShot(bnSnapShot* self);
 #endif
