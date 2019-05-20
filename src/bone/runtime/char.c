8e0454f54c93014908fe0732186420571736cc8e
Koya <koya0306@outlook.jp>
2019-05-20 14:53:34 +0900

    fix function parameter

--- src/bone/runtime/char.c
+++ src/bone/runtime/char.c
@@ -10,11 +10,13 @@ bnChar* bnNewChar(bnInterpreter* bone, char value) {
         ret->value = value;
         bnDefine(&ret->base, bnIntern(bone->pool, BN_KWD_EQUAL),
                  bnNewLambdaFromCFunc(bone, bnStdCharEqual, bone->pool,
-                                      BN_C_ADD_PARAM, "self", BN_C_ADD_RETURN,
-                                      "ret", BN_C_ADD_EXIT));
+                                      BN_C_ADD_PARAM, "self", BN_C_ADD_PARAM,
+                                      "other", BN_C_ADD_RETURN, "ret",
+                                      BN_C_ADD_EXIT));
         bnDefine(&ret->base, bnIntern(bone->pool, BN_KWD_NOTEQUAL),
                  bnNewLambdaFromCFunc(bone, bnStdCharNotEqual, bone->pool,
-                                      BN_C_ADD_PARAM, "self", BN_C_ADD_RETURN,
-                                      "ret", BN_C_ADD_EXIT));
+                                      BN_C_ADD_PARAM, "self", BN_C_ADD_PARAM,
+                                      "other", BN_C_ADD_RETURN, "ret",
+                                      BN_C_ADD_EXIT));
         return ret;
 }
