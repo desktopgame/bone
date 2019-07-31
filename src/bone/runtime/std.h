a9aa261635ebdf8507b4a4168f03669ef7d5b542
desktopgame <gnndr36@gmail.com>
2019-07-31 14:59:19 +0900

    refactor

--- src/bone/runtime/std.h
+++ src/bone/runtime/std.h
@@ -138,55 +138,6 @@ void bnStdBoolEqual(struct bnInterpreter* bone, struct bnFrame* frame);
 
 void bnStdBoolNotEqual(struct bnInterpreter* bone, struct bnFrame* frame);
 
-// Integer
-void bnStdIntegerFuncCall(struct bnInterpreter* bone, struct bnFrame* frame);
-
-void bnStdIntegerPositive(struct bnInterpreter* bone, struct bnFrame* frame);
-
-void bnStdIntegerNegative(struct bnInterpreter* bone, struct bnFrame* frame);
-
-void bnStdIntegerChilda(struct bnInterpreter* bone, struct bnFrame* frame);
-
-void bnStdIntegerNot(struct bnInterpreter* bone, struct bnFrame* frame);
-
-void bnStdIntegerPlus(struct bnInterpreter* bone, struct bnFrame* frame);
-
-void bnStdIntegerMinus(struct bnInterpreter* bone, struct bnFrame* frame);
-
-void bnStdIntegerMultiply(struct bnInterpreter* bone, struct bnFrame* frame);
-
-void bnStdIntegerDivide(struct bnInterpreter* bone, struct bnFrame* frame);
-
-void bnStdIntegerModulo(struct bnInterpreter* bone, struct bnFrame* frame);
-
-void bnStdIntegerBitAnd(struct bnInterpreter* bone, struct bnFrame* frame);
-
-void bnStdIntegerBitOr(struct bnInterpreter* bone, struct bnFrame* frame);
-
-// void bnStdIntegerLogicAnd(struct bnInterpreter* bone, struct bnFrame* frame);
-
-// void bnStdIntegerLogicOr(struct bnInterpreter* bone, struct bnFrame* frame);
-
-void bnStdIntegerExcOr(struct bnInterpreter* bone, struct bnFrame* frame);
-
-void bnStdIntegerLShift(struct bnInterpreter* bone, struct bnFrame* frame);
-
-void bnStdIntegerRShift(struct bnInterpreter* bone, struct bnFrame* frame);
-
-void bnStdIntegerGT(struct bnInterpreter* bone, struct bnFrame* frame);
-
-void bnStdIntegerGE(struct bnInterpreter* bone, struct bnFrame* frame);
-
-void bnStdIntegerLT(struct bnInterpreter* bone, struct bnFrame* frame);
-
-void bnStdIntegerLE(struct bnInterpreter* bone, struct bnFrame* frame);
-
-void bnStdIntegerEqual(struct bnInterpreter* bone, struct bnFrame* frame);
-
-void bnStdIntegerNotEqual(struct bnInterpreter* bone, struct bnFrame* frame);
-
-void bnStdIntegerToString(struct bnInterpreter* bone, struct bnFrame* frame);
-
 // String
 
 void bnStdStringFuncCall(struct bnInterpreter* bone, struct bnFrame* frame);
