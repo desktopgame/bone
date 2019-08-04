8aaf6376cdf03c80dcb71c5b71f742e971951b42
Koya <koya0306@outlook.jp>
2019-08-04 19:22:02 +0900

    updae io.c

--- src/bone/util/io.c
+++ src/bone/util/io.c
@@ -11,11 +11,11 @@ void bnInitIO() {
         in = stdin;
 }
 
-FILE* bnStdout() { return out; }
+FILE* bnStdout() { return out == NULL ? stdout : out; }
 
-FILE* bnStderr() { return err; }
+FILE* bnStderr() { return err == NULL ? stderr : err; }
 
-FILE* bnStdin() { return in; }
+FILE* bnStdin() { return in == NULL ? stdin : in; }
 
 bool bnExists(const char* path) {
         FILE* fp = fopen(path, "r");
