e3aab9b4b7d2e019f99f168f3c3f01f1bcf3ee18
Koya <koya0306@outlook.jp>
2019-08-13 20:05:01 +0900

    refactor

--- src/bone/il/il_expr_lambda.c
+++ src/bone/il/il_expr_lambda.c
@@ -4,7 +4,6 @@
 #include "../runtime/keyword.h"
 #include "il_statement.h"
 
-static bool is_instance_base(struct bnStringPool* pool, bnILExprLambda* self);
 static void delete_il_statement(gpointer data);
 
 bnILExprLambda* bnNewILExprLambda(bnStringView filename, int lineno) {
@@ -25,7 +24,8 @@ void bnDumpILExprLambda(FILE* fp, struct bnStringPool* pool,
         bnFindent(fp, depth + 1);
         fprintf(fp, "parameters\n");
         for (int i = 0; i < self->parameters->len; i++) {
-                bnStringView param = g_ptr_array_index(self->parameters, i);
+                bnStringView param =
+                    (bnStringView)g_ptr_array_index(self->parameters, i);
                 bnFindent(fp, depth + 2);
                 fprintf(fp, "%s\n", bnView2Str(pool, param));
         }
@@ -33,7 +33,8 @@ void bnDumpILExprLambda(FILE* fp, struct bnStringPool* pool,
         bnFindent(fp, depth + 1);
         fprintf(fp, "returns\n");
         for (int i = 0; i < self->returns->len; i++) {
-                bnStringView param = g_ptr_array_index(self->returns, i);
+                bnStringView param =
+                    (bnStringView)g_ptr_array_index(self->returns, i);
                 bnFindent(fp, depth + 2);
                 fprintf(fp, "%s\n", bnView2Str(pool, param));
         }
@@ -52,17 +53,20 @@ void bnGenerateILExprLambda(bnInterpreter* bone, bnILExprLambda* self,
         bnWriteCode(env, self->lineno);
         bnWriteCode(env, self->parameters->len);
         for (int i = 0; i < self->parameters->len; i++) {
-                bnStringView name = g_ptr_array_index(self->parameters, i);
+                bnStringView name =
+                    (bnStringView)g_ptr_array_index(self->parameters, i);
                 bnWriteCode(env, name);
         }
         bnWriteCode(env, self->returns->len);
         for (int i = 0; i < self->returns->len; i++) {
-                bnStringView name = g_ptr_array_index(self->returns, i);
+                bnStringView name =
+                    (bnStringView)g_ptr_array_index(self->returns, i);
                 bnWriteCode(env, name);
         }
         bnGenerateEnterLambda(env);
         for (int i = 0; i < self->parameters->len; i++) {
-                bnStringView name = g_ptr_array_index(self->parameters, i);
+                bnStringView name =
+                    (bnStringView)g_ptr_array_index(self->parameters, i);
                 bnWriteCode(env, BN_OP_STORE);
                 bnWriteCode(env, name);
         }
@@ -80,15 +84,6 @@ void bnDeleteILExprLambda(bnILExprLambda* self) {
         g_ptr_array_free(self->returns, TRUE);
         g_ptr_array_free(self->statements, TRUE);
         BN_FREE(self);
-}
-
-static bool is_instance_base(struct bnStringPool* pool, bnILExprLambda* self) {
-        guint len = g_list_length(self->parameters);
-        if (len == 0) {
-                return false;
-        }
-        bnStringView name = g_ptr_array_index(self->parameters, 0);
-        return name == bnIntern(pool, BN_KWD_SELF);
 }
 
 static void delete_il_statement(gpointer data) {
