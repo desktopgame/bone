54da270d81bd030b9a4bc200cb4ea092b7b7e49e
Koya <koya0306@outlook.jp>
2019-08-19 22:21:29 +0900

    add comment

--- src/bone/util/args.h
+++ src/bone/util/args.h
@@ -2,9 +2,22 @@
 #define BONE_UTIL_ARGS_H
 #include "../glib.h"
 
+/**
+ * argc, argv を保存してグローバルに参照可能にします。
+ * @param argc
+ * @param argv
+ */
 void bnInitArgs(int argc, char* argv[]);
 
+/**
+ * argcを返します。
+ * @return
+ */
 int bnArgc();
 
+/**
+ * argvを返します。
+ * @return
+ */
 char** bnArgv();
 #endif
