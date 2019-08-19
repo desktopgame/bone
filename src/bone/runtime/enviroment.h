f423bc864655cce411a65614970f18ff06e97273
Koya <koya0306@outlook.jp>
2019-08-19 23:43:35 +0900

    add comment

--- src/bone/runtime/enviroment.h
+++ src/bone/runtime/enviroment.h
@@ -7,6 +7,10 @@
 
 struct bnLambda;
 
+/**
+ * bnEnviromentはバイトコード、ジャンプテーブルなどのバイトコード生成が
+ * 完了してから不変なデータを保存する構造体です。
+ */
 typedef struct bnEnviroment {
         bnStringView filename;
         GPtrArray* codeArray;
@@ -18,25 +22,28 @@ typedef struct bnEnviroment {
 } bnEnviroment;
 
 /**
- * return new instance of bnEnviroment.
+ * 新しいbnEnviromentインスタンスを生成して返します。
  * @param filename
  * @return
  */
 bnEnviroment* bnNewEnviroment(bnStringView filename);
 
 /**
+ * 末尾にオペコードを一つ書き出します。
  * @param self
  * @param code
  */
 void bnWriteCode(bnEnviroment* self, int code);
 /**
+ * 指定位置からオペコードをひとつ読み出します。
  * @param self
- * @param @c
+ * @param pc
  * @return
  */
 int bnReadCode(bnEnviroment* self, int pc);
 
 /**
+ * 指定位置からラベルをひとつ読み出します。
  * @param self
  * @param pc
  * @return
@@ -44,18 +51,24 @@ int bnReadCode(bnEnviroment* self, int pc);
 bnLabel* bnReadLabel(bnEnviroment* self, int pc);
 
 /**
+ * 末尾へラベルを書き出します。
+ * ラベルはポインタをそのまま保存します。
+ * これはif文などのコード生成で後方へジャンプする必要が生じるためです。
  * @param self
  * @param label
  */
 void bnWriteLabel(bnEnviroment* self, bnLabel* label);
 
 /**
+ * 行範囲を追加します。オペコードから行番号を逆算するために、
+ * まとまったコード生成が終わるたびに呼び出します。
  * @param self
  * @param lineno
  */
 void bnAddLineRange(bnEnviroment* self, int lineno);
 
 /**
+ * オペコードから行番号を逆算します。
  * @param self
  * @param pc
  * @return
@@ -63,6 +76,7 @@ void bnAddLineRange(bnEnviroment* self, int lineno);
 int bnFindLineRange(bnEnviroment* self, int pc);
 
 /**
+ * 指定の位置を指すラベルを生成して書き出します。
  * @param self
  * @param pos
  * @return
@@ -70,6 +84,7 @@ int bnFindLineRange(bnEnviroment* self, int pc);
 bnLabel* bnAutoNewLabel(bnEnviroment* self, int pos);
 
 /**
+ * ラムダによるオフセットを加算した上でラベルを書き出します。
  * @param self
  * @param pos
  * @return
@@ -77,41 +92,50 @@ bnLabel* bnAutoNewLabel(bnEnviroment* self, int pos);
 bnLabel* bnGenerateLabel(bnEnviroment* self, int pos);
 
 /**
+ * 末尾にNOP(No OPeration)を書き出して位置を返します。
+ * ジャンプ先として便利です。
  * @param self
  * @return
  */
 int bnGenerateNOP(bnEnviroment* self);
 
 /**
+ * 指定の数だけNOP(No OPeration)を書き出します。
  * @param self
  * @param count
  */
 void bnGenerateFillNOP(bnEnviroment* self, int count);
 
 /**
+ * ラムダ式の内部のコード生成において、
+ * 正しいラベル位置を計算するためにラムダ式のコード生成の最初で呼び出します。
  * @param self
  */
 void bnGenerateEnterLambda(bnEnviroment* self);
 
 /**
+ * ラムダ式の内部のコード生成において、
+ * 正しいラベル位置を計算するためにラムダ式のコード生成の最後で呼び出します。
  * @param self
  */
 void bnGenerateExitLambda(bnEnviroment* self);
 
 /**
+ * 最後のラムダ式からのオフセットを返します。
  * @param self
  * @return
  */
 int bnGetLambdaOffset(bnEnviroment* self);
 
 /**
+ * 最後のラムダ式からのオフセットを返します。
  * @param self
  * @return
  */
 int bnGetPrependPos(bnEnviroment* self);
 
 /**
- * free a bnEnviroment.
+ * 全てのオペコードやジャンプテーブルを解放します。
  * @param self
  */
 void bnDeleteEnviroment(bnEnviroment* self);
