bfafe6c74367724c5769cf463be97b3c72c359b8
Koya <koya0306@outlook.jp>
2019-08-19 23:27:47 +0900

    add comment

--- src/bone/runtime/array.h
+++ src/bone/runtime/array.h
@@ -3,7 +3,7 @@
 #include "object.h"
 
 /**
- * return new instance of bnArray.
+ * bnArrayインスタンスを生成し、参照を返します。
  * @param bone
  * @param size
  * @return
@@ -11,6 +11,7 @@
 bnReference bnNewArray(struct bnInterpreter* bone, int size);
 
 /**
+ * 指定の配列をbnCharインスタンスで埋めます。
  * @param bone
  * @param str
  * @param ref
@@ -18,12 +19,14 @@ bnReference bnNewArray(struct bnInterpreter* bone, int size);
 void bnFillString(struct bnInterpreter* bone, const char* str, bnReference ref);
 
 /**
+ * 指定の配列の長さを返します。
  * @param obj
  * @return
  */
 int bnGetArrayLength(bnObject* obj);
 
 /**
+ * 指定の配列の指定位置のインスタンスへの参照を返します。
  * @param obj
  * @param index
  * @return
@@ -31,6 +34,7 @@ int bnGetArrayLength(bnObject* obj);
 bnReference bnGetArrayElementAt(bnObject* obj, int index);
 
 /**
+ * 指定の配列の指定位置のインスタンスへの参照を上書きします。
  * @param obj
  * @param index
  * @param ref
