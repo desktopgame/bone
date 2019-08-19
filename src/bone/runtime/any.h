2f0abeac867164e59cd0cbf60ba9d1fa23c6ddae
Koya <koya0306@outlook.jp>
2019-08-19 23:25:25 +0900

    add comment

--- src/bone/runtime/any.h
+++ src/bone/runtime/any.h
@@ -3,19 +3,25 @@
 #include "object.h"
 struct bnInterpreter;
 
+/**
+ * bnAnyは拡張ライブラリのための構造体です。
+ * 独自に定義される構造体はこれにキャスト可能なポインタでなければなりません。
+ */
 typedef struct bnAny {
         bnObject base;
         bnStringView type;
 } bnAny;
 
 /**
+ * bnAnyインスタンスを初期化します。
  * @param bone
  * @param self
- * @param type
+ * @param type 拡張ライブラリの指定の型を一意に表す名前
  */
 void bnInitAny(struct bnInterpreter* bone, bnAny* self, const char* type);
 
 /**
+ * bnAnyが指定のタイプであるかどうか判別します。
  * @param bone
  * @param self
  * @parram type
