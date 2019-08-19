f2ac4642c861c627a0dfb32fa3bc1330d2721a51
Koya <koya0306@outlook.jp>
2019-08-19 23:57:45 +0900

    add comment

--- src/bone/runtime/heap.h
+++ src/bone/runtime/heap.h
@@ -11,17 +11,21 @@ struct bnInterpreter;
 #define OBJECT_MAXSIZE (48)
 
 /**
+ * 新しいbnHeapインスタンスを生成して返します。
  * @return
  */
 struct bnHeap* bnNewHeap();
 
 /**
+ * ヒープからまだ使用されていないオブジェクトを検索し、
+ * そのオブジェクトへの参照を返します。
  * @param self
  * @return
  */
 bnReference bnAllocObject(struct bnHeap* self);
 
 /**
+ * 参照をオブジェクトの実際のポインタへ変換します。
  * @param self
  * @param ref
  * @return
@@ -29,21 +33,22 @@ bnReference bnAllocObject(struct bnHeap* self);
 void* bnGetObject(struct bnHeap* self, bnReference ref);
 
 /**
+ * 参照を未使用としてマークします。
  * @parram self
  * @param ref
  */
 void bnFreeObject(struct bnHeap* self, bnReference ref);
 
 /**
- * bnPushStage is push new stage.
- * bnStaging is will record objects to created last stage by this function.
+ * 新たなステージを生成してプッシュします。
  * @param self
  */
 void bnPushStage(struct bnHeap* self);
 
 /**
- * bnStaging is will register object to last stage.
- * was registered object is not collect by GC until it stage poped.
+ * 最後に生成されたステージに参照を追加します。
+ * ステージに追加された参照はGCから保護されます。
+ * 主にネイティブメソッドのための仕組みです。
  * @param self
  * @param ref
  * @return
@@ -51,24 +56,32 @@ void bnPushStage(struct bnHeap* self);
 bnReference bnStaging(struct bnHeap* self, bnReference ref);
 
 /**
- * bnPopStage is pop last stage.
- * should be call at finished all staging process.
+ * 最後に生成されたステージを解放します。
  * @param self
  */
 void bnPopStage(struct bnHeap* self);
 
 /**
+ * ガベージコレクションとコンパクションを実行します。
+ * この呼び出しの前に参照からポインタを得ていた場合、
+ * コンパクションによってポインタの意味する値が変わってしまっているため、
+ * 再びbnGetObjectによってポインタを取得する必要があります。
  * @param bone
  */
 void bnGC(struct bnInterpreter* bone);
 
 /**
+ * ヒープが内部的に使用しているストレージを返します。
+ * この関数を呼び出すのは好ましくありません。
+ * bnAllocObject,bnGetObject,bnFreeObjectで十分なはずです。
+ * 主にデバッグのために提供されている関数です。
  * @param self
  * @return
  */
 bnStorage* bnGetHeapStorage(struct bnHeap* self);
 
 /**
+ * オブジェクトを格納するための領域を解放します。
  * @param self
  */
 void bnDeleteHeap(struct bnHeap* self);
