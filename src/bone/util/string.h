5cda209efe0de112c5dc05e53389aab80e499ebc
Koya <koya0306@outlook.jp>
2019-08-19 23:05:14 +0900

    add comment

--- src/bone/util/string.h
+++ src/bone/util/string.h
@@ -2,7 +2,8 @@
 #define BONE_UTIL_STRING_H
 
 /**
- * return a position of last component in filepath.
+ * 文字列を後ろから辿って、最初に / が見つかった位置を返します。
+ * つまり、パスからファイル名を取得するために使えます。
  * @param path
  * @return
  */
