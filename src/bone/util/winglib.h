7d4b932f05faafc6c6203c99487534c2fd2f7bfb
Koya <koya0306@outlook.jp>
2019-08-19 23:07:23 +0900

    add comment

--- src/bone/util/winglib.h
+++ src/bone/util/winglib.h
@@ -3,6 +3,8 @@
 #if _MSC_VER
 struct GString;
 /**
+ * g_string_append_cの代わりにg_string_appendを呼び出す関数です。
+ * Windows環境ではg_string_append_cが正しく動作しないために用意しました。
  * @param str
  * @param c
  * @return
@@ -10,6 +12,13 @@ struct GString;
 struct GString* bnGStringAppendC(struct GString* str, char c);
 #else
 
+/**
+ * g_string_append_cのラッパーです。
+ * Windows環境では異なる実装に置き換えられます。
+ * @param str
+ * @param c
+ * @return
+ */
 #define bnGStringAppendC(str, c) (g_string_append_c(str, c))
 #endif
 #endif
