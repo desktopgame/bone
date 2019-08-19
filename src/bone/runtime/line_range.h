67a3c02565610e807c83a52b40509dbe0eaca2f9
Koya <koya0306@outlook.jp>
2019-08-20 00:26:59 +0900

    add comment

--- src/bone/runtime/line_range.h
+++ src/bone/runtime/line_range.h
@@ -1,13 +1,24 @@
 #ifndef BONE_RUNTIME_LINERANGE_H
 #define BONE_RUNTIME_LINERANGE_H
 
+/**
+ * bnLineRangeはオペコードのどこからどこまでがどの行に対応しているかを表す構造体です。
+ */
 typedef struct bnLineRange {
         int start;
         int end;
         int line;
 } bnLineRange;
 
+/**
+ * 新しいbnLineRangeインスタンスを生成して返します。
+ * @return
+ */
 bnLineRange* bnNewLineRange();
 
+/**
+ * ラインレンジを解放します。
+ * @param self
+ */
 void bnDeleteLineRange(bnLineRange* self);
 #endif
