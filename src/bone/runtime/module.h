7408a96d55209a172dd62ab7042186003e7fbf13
Koya <koya0306@outlook.jp>
2019-08-20 00:29:25 +0900

    add comment

--- src/bone/runtime/module.h
+++ src/bone/runtime/module.h
@@ -2,14 +2,34 @@
 #define BONE_RUNTIME_MODULE_H
 #include "../glib.h"
 
+/**
+ * bnModuleは動的リンクライブラリへのハンドルを保存する構造体です。
+ */
 typedef struct bnModule {
         char* path;
         void* handle;
 } bnModule;
 
+/**
+ * 指定のパスの動的リンクライブラリをロードして、
+ * 新しいbnModuleインスタンスを返します。
+ * @param path
+ * @return
+ */
 bnModule* bnNewModule(const char* path);
 
+/**
+ * 指定の名前でシンボルを読み込んで返します。
+ * @param self
+ * @param name
+ * @return
+ */
 void* bnGetSymbol(bnModule* self, const char* name);
 
+/**
+ * ハンドルをクローズして、
+ * モジュールを解放します。
+ * @param self
+ */
 void bnDeleteModule(bnModule* self);
 #endif
