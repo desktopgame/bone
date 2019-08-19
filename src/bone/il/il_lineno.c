11d77c34e424fbd5ae9f6bb8c40bc11aaa0863be
Koya <koya0306@outlook.jp>
2019-08-19 19:21:35 +0900

    fix il_lineno.c

--- src/bone/il/il_lineno.c
+++ src/bone/il/il_lineno.c
@@ -1,14 +1,20 @@
 #include "il_lineno.h"
 #include <stdbool.h>
 
+#if !defined(NGLOBAL)
 static int gLine = -1;
+#endif
 
 static bool showLine(int line) {
+#if defined(NGLOBAL)
+        return false;
+#else
         if (gLine == line) {
                 return false;
         }
         gLine = line;
         return true;
+#endif
 }
 
 void bnDumpLineno(FILE* fp, int line) {
