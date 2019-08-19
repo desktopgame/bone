bb7c43db722348008dd1cf6674bb1e115fb17f4d
Koya <koya0306@outlook.jp>
2019-08-20 00:50:13 +0900

    add comment

--- src/bone/runtime/string.h
+++ src/bone/runtime/string.h
@@ -4,7 +4,7 @@
 #include "object.h"
 
 /**
- * return new instance of bnString.
+ * 新しい文字列インスタンスを生成して、参照を返します。
  * @param bone
  * @param name
  * @return
@@ -12,6 +12,7 @@
 bnReference bnNewString(bnInterpreter* bone, bnStringView value);
 
 /**
+ * 新しい文字列インスタンスを生成して、参照を返します。
  * @param bone
  * @param str
  * @return
@@ -19,6 +20,7 @@ bnReference bnNewString(bnInterpreter* bone, bnStringView value);
 bnReference bnNewString2(bnInterpreter* bone, const char* str);
 
 /**
+ * 文字列プールで使用可能な値に変換します。
  * @param obj
  * @return
  */
