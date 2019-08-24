b9bffa7bdcfc227be21f002f0eb18037ba547d23
Koya <koya0306@outlook.jp>
2019-08-24 20:51:57 +0900

    fix for windows

--- src/bone/util/string.c
+++ src/bone/util/string.c
@@ -1,11 +1,17 @@
 ﻿#include "string.h"
 #include <string.h>
 
+#if _MSC_VER
+#define SLASH ('\\')
+#else
+#define SLASH ('/')
+#endif
+
 int bnLastPathComponent(const char* path) {
         int len = strlen(path);
         int pos = 0;
         const char* sp = path + (len - 1);
-        while (pos < len && *sp != '/') {
+        while (pos < len && *sp != SLASH) {
                 sp--;
                 pos++;
         }
